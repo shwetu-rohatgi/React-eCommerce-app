258824d525afd165e63891a28f72d19e19be611f
Shwetanshu Rohatgi <shwetanshu.rohatgi@gmail.com>
2020-04-18 11:39:16 -0700

    Built Menu-Item and Directory component

---
+++ src/components/menu-items/menu-items.component.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable no-template-curly-in-string */
+import React from 'react';
+import './menu-items.styles.scss';
+
+const MenuItem = ({ title,imgUrl,size }) => (
+    <div className={ `${size} menu-item` }>
+        <div className="background-image" style={{ backgroundImage: `url(${imgUrl})`}}/>
+        <div className="content">
+            <h1 className="title">{ title.toUpperCase() }</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
